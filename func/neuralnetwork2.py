334918651747012adfb5db6b6f063b0bcf4b2b90
kebertj8 <Kebertj8@gmail.com>
2020-07-10 14:20:39 -0400

    neural networking imaging

---
+++ func/neuralnetwork2.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+from tensorflow import keras
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images,
+                               test_labels) = fashion_mnist.load_data()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+
+])
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='sparse_categorical_crossentropy')
+
+# loss calculates how bad the neural network works and then
+#  the optimizer runs the program again to get it even better
+
+
+# fit the training images to the training labels
+
+model.fit(train_images, train_labels, epochs=5)
+
+# testing the model with images it hasnt previoulsy seen and see how it performs
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+#  then we get call to see if it recongizes the images.
+
+predictions = model.predict(my_images)
+
+# basically printing the function
